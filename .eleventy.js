ac1cc8cb0e6e6f140b9875a066caa182e580c7b0
stevejamesson <steve@evendev.com>
2018-08-17 16:32:21 -0400

    misc

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,22 @@
+const _ = require('lodash')
+
 module.exports = function(config) {
     config.dir = {
         input: 'src',
         output: '_site'
     }
 
+    // Passthrough Copy
     config.addPassthroughCopy('src/assets')
+
+    // Collections
+    function getOfferings(collection, tag) {
+      return _.sortBy(collection.getFilteredByTag(tag), 'fileSlug')
+    }
+    // Offerings
+    config.addCollection('offerings_branding', c => getOfferings(c, 'branding'))
+    config.addCollection('offerings_marketing', c => getOfferings(c, 'marketing'))
+    config.addCollection('offerings_web', c => getOfferings(c, 'web'))
 
     return config
 }
