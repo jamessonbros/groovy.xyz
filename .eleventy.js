a0e37fb4349c9f84c52aecdabe11215a48aaac3f
stevejamesson <steve@evendev.com>
2018-08-17 16:34:48 -0400

    clean up for prod branch

--- .eleventy.js
+++ .eleventy.js
@@ -9,14 +9,5 @@ module.exports = function(config) {
     // Passthrough Copy
     config.addPassthroughCopy('src/assets')
 
-    // Collections
-    function getOfferings(collection, tag) {
-      return _.sortBy(collection.getFilteredByTag(tag), 'fileSlug')
-    }
-    // Offerings
-    config.addCollection('offerings_branding', c => getOfferings(c, 'branding'))
-    config.addCollection('offerings_marketing', c => getOfferings(c, 'marketing'))
-    config.addCollection('offerings_web', c => getOfferings(c, 'web'))
-
     return config
 }
